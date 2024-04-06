9f56941219441a84809dffe71ad7f1ac16064b3d
Gerald Stevens <gerbob@gmail.com>
2024-04-05 19:54:11 -0600

    Adding vehicles index

---
+++ MaintenanceLog/DbUtils.cs
@@ -0,0 +1,33 @@
+﻿using MaintenanceLog.DataAccess;
+using MaintenanceLog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaintenanceLog
+{
+    public static class DbUtils
+    {
+        public static async Task EnsureDbCreated(DbContextOptions<MaintenanceLogContext> options)
+        {
+            var builder = new DbContextOptionsBuilder<MaintenanceLogContext>(options)
+                .UseLoggerFactory(new LoggerFactory());
+
+            using var context = new MaintenanceLogContext(builder.Options);
+
+            if (await context.Database.EnsureCreatedAsync())
+            {
+                await AddTestVehicles(context);
+            }
+        }
+
+        public static async Task AddTestVehicles(MaintenanceLogContext context)
+        {
+            var xterra = new Vehicle { Make = "Nissan", Model = "XTerra", Name = "Bouncy", Year = 2012 };
+            var ion = new Vehicle { Make = "Saturn", Model = "Ion", Name = "Wheezy", Year = 2007 };
+
+            context.Vehicles.Add(xterra);
+            context.Vehicles.Add(ion);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ MaintenanceLog/Models/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿namespace MaintenanceLog.Models
+{
+    public class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public string? Name { get; set; }
+        public int Year { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+    }
+}
